c30b9fe87d04f2a260a6ecc6ac92553d74d9fbfb
willie <chi8857@gmail.com>
2018-11-29 14:15:22 +0800

    add

--- MsgSend.py
+++ MsgSend.py
@@ -30,7 +30,7 @@ except LineBotApiError as e:
 	raise e
 	
 try:
-	line_bot_api.push_message(to, '4444')
+	line_bot_api.push_message(to, TextSendMessage(text='https://github.com/willie-isom/isom-test/blob/master/1.xlsx?raw=true'))
 except LineBotApiError as e:
 	raise e
 
